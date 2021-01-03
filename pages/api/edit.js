0207808edbbc39212a83a74f7b8e69d8b47651a9
tejasra <56512902+tejasra@users.noreply.github.com>
2021-01-03 21:34:30 +0530

    Create edit.js

---
+++ pages/api/edit.js
@@ -0,0 +1,17 @@
+import db from "../../utils/dbConnect";
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    try {
+      let { id } = req.body;
+      const result = await db.result("SELECT * FROM NOTES WHERE ID=$1", [id]);
+      result.rowCount === 1
+        ? res.status(200).json({ user: result.rows })
+        : res.status(404).json({ user: false });
+    } catch (err) {
+      res.status(400).json({ user: err });
+    }
+  } else {
+    res.redirect("/");
+  }
+};
